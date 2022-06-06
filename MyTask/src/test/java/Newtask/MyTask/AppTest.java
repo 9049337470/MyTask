cadbe300060ccd47df5d81c09ef6060a5da745c3
Admin <Admin@LAPTOP-TMTB6ITF>
2022-06-06 09:54:03 +0530

    First Task

---
+++ MyTask/src/test/java/Newtask/MyTask/AppTest.java
@@ -0,0 +1,20 @@
+package Newtask.MyTask;
+
+import static org.junit.Assert.assertTrue;
+
+import org.junit.Test;
+
+/**
+ * Unit test for simple App.
+ */
+public class AppTest 
+{
+    /**
+     * Rigorous Test :-)
+     */
+    @Test
+    public void shouldAnswerWithTrue()
+    {
+        assertTrue( true );
+    }
+}
